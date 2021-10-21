701229790e977cbad34a5af6d35be2bf8dad5389
Laurajcb <callejaslaura96@gmail.com>
2021-10-21 23:14:41 +0000

    Added header and bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+* bubble_sort - Function that sorts an array of integers using
+* bubble sort algorithm.
+* @array: array of integers.
+* @size: array size.
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int i = 0;
+	int again = 1;
+	int tmp = 0;
+	int swap = 0;
+
+	while (again > 0)
+	{
+		for (i = 0, swap = 0; i < size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				swap += 1;
+				print_array(array, size);
+			}
+		}
+		if (swap == 0)
+		{
+			again = 0;
+		}
+	}
+}
