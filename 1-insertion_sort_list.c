260a6355fe95dc55678b92ad53905899a4d14597
Laurajcb <callejaslaura96@gmail.com>
2021-10-25 17:32:45 +0000

    added insetion and big O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+/**
+* insertion_sort_list-  function that sorts a doubly linked
+*list of integers in ascending order using the Insertion
+*sort algorithm
+* @list: list of integers.
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = NULL;
+	listint_t *previous = NULL;
+
+	if (list == NULL || *list == NULL)
+		return;
+	if ((*list)->next == NULL)
+	{
+		print_list(*list);
+	}
+	for (current = (*list)->next; current; current = current->next)
+	{
+		while (current->prev && current->prev->n > current->n)
+		{
+			previous = current->prev;
+			current->prev = previous->prev;
+			previous->prev = current;
+			previous->next = current->next;
+			current->next = previous;
+			if (current->prev)
+			{
+				current->prev->next = current;
+			}
+			else
+			{
+				*list = current;
+			}
+			if (previous->next)
+			{
+				previous->next->prev = previous;
+			}
+			print_list(*list);
+		}
+	}
+}
