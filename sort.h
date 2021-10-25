63f849722f4f5113ba606ebc195bab31397259ed
anversa-pro <angelavergaras@gmail.com>
2021-10-25 10:58:46 -0500

    [MODIFY] fix

--- sort.h
+++ sort.h
@@ -13,13 +13,22 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
+/* Prints an array  */
 void print_array(const int *array, size_t size);
+/*Prints a list */
 void print_list(const listint_t *list);
+/* sorts an array of int in ascending order with Bubble sort algorithm */
 void bubble_sort(int *array, size_t size);
+/* sorts a dll of integers in ascending order with Insertion sort algorithm */
+void insertion_sort_list(listint_t **list);
+/* sorts an array of int in ascending order with Selection sort algorithm */
+void selection_sort(int *array, size_t size);
+/* sorts an array of int in ascending order with Quick sort algorithm */
+void quick_sort(int *array, size_t size);
 
 #endif
