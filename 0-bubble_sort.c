79695ee511c72c17a023d60e6e10fdd04e70135b
Laurajcb <callejaslaura96@gmail.com>
2021-10-25 17:17:19 +0000

    fix conditional

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,7 +17,7 @@ void bubble_sort(int *array, size_t size)
 
 	while (again > 0)
 	{
-		for (i = 0, swap = 0; i < size; i++)
+		for (i = 0, swap = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
