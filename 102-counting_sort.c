faa678f07c3a89e6ba85d30c1255edae305b8533
anversa-pro <angelavergaras@gmail.com>
2021-10-26 17:07:36 -0500

    [MODIFY] variables types

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdlib.h>
 
 /**
 * counting_sort- that sorts an array of integers in
@@ -8,12 +9,13 @@
 */
 void counting_sort(int *array, size_t size)
 {
-	int i = 0, tmp = 0, tmp2 = 0, max = 0;
-	int *aux_array, *new_array;
+	int i = 0, tmp_size = (int)size;
+	unsigned int tmp = 0, tmp2 = 0;
+	int *aux_array, *new_array, max = 0;
 
 	if (size < 2 || array == NULL)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < tmp_size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
@@ -21,27 +23,24 @@ void counting_sort(int *array, size_t size)
 	aux_array = calloc((max + 1), sizeof(int));
 	if (aux_array == NULL)
 		return;
-	for (i = size - 1; i >= 0; i--)
+	for (i = tmp_size - 1; i >= 0; i--)
 	{
 		tmp = array[i];
 		aux_array[tmp] = aux_array[tmp] + 1;
 	}
 	for (i = 1; i <= max; i++)
 		aux_array[i] += aux_array[i - 1];
-
 	print_array(aux_array, max + 1);
-
-	new_array = calloc(size, sizeof(int));
+	new_array = calloc(tmp_size, sizeof(int));
 	if (new_array == NULL)
 		return;
-	for (i = size - 1; i >= 0; i--)
+	for (i = tmp_size - 1; i >= 0; i--)
 	{
 		tmp2 = array[i];
 		new_array[aux_array[tmp2] - 1] = tmp2;
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < tmp_size; i++)
 		array[i] = new_array[i];
-
 	free(aux_array);
 	free(new_array);
 }
